915936e727b8d1664750a076623ce052ab872959
Pankaj <pankaj.ky3007@gmail.com>
2023-10-01 00:18:15 +0530

    Navbar Added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* add event listeners here */
+const hamburger = document.querySelector(".hamberg");
+const menu = document.querySelector(".menu");
+
+let isOpen = false;
+
+hamburger.addEventListener('click', function () {
+    if (!isOpen) {
+        menu.style.display = "block";
+        isOpen = true;
+    } else {
+        menu.style.display = "none";
+        isOpen = false;
+    }
+});
